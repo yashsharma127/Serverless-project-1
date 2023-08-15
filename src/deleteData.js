a030d059fe5bb33238e6728c8c7dc98f0bae2a1a
yashsharma127 <yashshar.2323@gmail.com>
2023-08-15 18:43:51 +0530

    adding delete file

---
+++ src/deleteData.js
@@ -0,0 +1,27 @@
+"use strict";
+const AWS = require("aws-sdk");
+
+const deleteData = async (event) => {
+
+  const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+  const { completed } = JSON.parse(event.body);
+  const { id } = event.pathParameters;
+
+  await dynamoDb.update({
+    TableName: "dataTable",
+    Key: { id },
+    UpdateExpression: "set completed = :completed",
+    ExpressionAttributeValues: {":completed": completed},
+    ReturnValues: "ALL_NEW"
+  }).promise();
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({msg:"Kaam Khatam Kar Diya"}),
+    };
+};
+
+module.exports = {
+    handler: deleteData,
+};
