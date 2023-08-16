f7393272030833124f1a185bc3828e7563111e78
yashsharma127 <yashshar.2323@gmail.com>
2023-08-16 19:12:19 +0530

    Updating names and modules

--- src/finishWork.js
+++ src/finishWork.js
@@ -1,7 +1,7 @@
 "use strict";
 const AWS = require("aws-sdk");
 
-const deleteData = async (event) => {
+const finishWork = async (event) => {
 
   const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
@@ -23,5 +23,5 @@ const deleteData = async (event) => {
 };
 
 module.exports = {
-    handler: deleteData,
+    handler: finishWork,
 };
